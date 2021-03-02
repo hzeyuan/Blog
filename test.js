b11a045fe1a3aa67b39e28b95c5bee3ce5ed9cc6
SharkNet <sharknet@SharkNets-Mac-mini.local>
2021-03-02 21:30:50 +0800

    h

--- test.js
+++ test.js
@@ -25,7 +25,8 @@ function add(a, b) {
 var fn = curry(function(a, b, c) {
     console.log([a, b, c]);
 })
+
 // fn("a", "b", "c") // ["a", "b", "c"]
-// fn("a", "b")("c") // ["a", "b", "c"]
+fn("a", "b")("c") // ["a", "b", "c"]
 fn("a")("b")("c") // ["a", "b", "c"]
 // fn("a")("b", "c") // ["a", "b", "c"]
